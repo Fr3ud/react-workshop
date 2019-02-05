98e0b99cb33cf22370762aca0ba7ef5f93aa141e
Fr3ud <oo.pavelko@gmail.com>
2019-02-05 23:53:32 +0200

    Initial commit

---
+++ source/index.js
@@ -0,0 +1,10 @@
+// Core
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Theme
+import './theme/init';
+
+const start = <h1 className = 'welcome'>Добро пожаловать!</h1>;
+
+ReactDOM.render(start, document.getElementById('app'));
